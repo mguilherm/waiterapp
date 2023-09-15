5e98baef1be156fda3972b452c72449fd369718d
Marcos Guilherme Silva <marcosguilherme.silva@outlook.com>
2023-09-15 19:28:18 -0300

    infra/mongo and express added to server

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(()=> {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`🔥 server is running on http://localhost:${port}`);
+    });
+    console.log('conectado ao mongo');
+  })
+  .catch(()=> console.log('Erro ao conectar ao mongo'));
+
+
+
+
+
+
